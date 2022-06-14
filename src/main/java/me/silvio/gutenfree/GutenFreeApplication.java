aa28ecac98d612903022e80ac2e755390636226c
Silvio Pastore <silvioppastore@gmail.com>
2022-06-14 15:30:53 +0200

    0.0.1 snapshot eclipse
    eclipse project creation

---
+++ src/main/java/me/silvio/gutenfree/GutenFreeApplication.java
@@ -0,0 +1,13 @@
+package me.silvio.gutenfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutenFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutenFreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/silvio/gutenfree/GutenFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.silvio.gutenfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutenFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
